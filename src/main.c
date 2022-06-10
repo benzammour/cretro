07bf45e0a1814f49cf8b9805629b1f378773b367
Samir Benzammour <samir.benzammour@gmail.com>
2022-06-10 12:01:23 +0200

    feat: init project skeleton

---
+++ src/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(int argc, char** argv) {
+	if (argc != 2) {
+		fprintf(stderr, "Usage: ./ch8 <ROM>\n");
+	}
+
+	const char* romFileName = argv[1];
+
+	fprintf(stdout, "TODO\n");
+
+	return 0;
+}
