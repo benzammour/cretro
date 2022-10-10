cd8928173ca77ff17fb6f6f2b61bd30c4bcc29db
Moritz Reinhard Hüllmann <mhuellmann@googlemail.com>
2022-10-10 20:40:49 +0200

    chore: touch debug.c/h

---
+++ src/debug.h
@@ -0,0 +1,15 @@
+/**
+ *
+ * @file	debug.h
+ * @brief 	Header file for debugging window
+ * @author 	Moritz R. Huellmann
+ * @date 	10.10.22
+ *
+ */
+
+#ifndef _CH8_SRC_DEBUG_H_
+#define _CH8_SRC_DEBUG_H_
+
+
+
+#endif /* _CH8_SRC_DEBUG_H_ */
