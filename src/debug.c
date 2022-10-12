ef1f1a5540cb5819cd5487d6222e21fc89b879b7
Moritz Reinhard Hüllmann <mhuellmann@googlemail.com>
2022-10-12 19:59:21 +0200

    chore: make enums uppercase

--- src/debug.c
+++ src/debug.c
@@ -4,18 +4,18 @@
 
 #include "debug.h"
 
-static debug_level_t min_dbg_lvl = fatal;
+static debug_level_t min_dbg_lvl = FATAL;
 
 void debug_init(const config_t* conf) {
     min_dbg_lvl = conf->debug;
 
     // minimum debug level cannot be higher than fatal
-    if (min_dbg_lvl > fatal)
-        min_dbg_lvl = fatal;
+    if (min_dbg_lvl > FATAL)
+        min_dbg_lvl = FATAL;
 
     // minimum debug level cannot be lower than debug
-    if (min_dbg_lvl < debug)
-        min_dbg_lvl = debug;
+    if (min_dbg_lvl < DEBUG)
+        min_dbg_lvl = DEBUG;
 
     LOG_INFO("Log level initialized to %d.", min_dbg_lvl);
 }
@@ -36,23 +36,23 @@ __attribute__((format(printf, 2, 3))) void _log_str(debug_level_t dbg_lvl, const
 
     const char* dbg_lvl_str = "unknown";
     switch (dbg_lvl) {
-        case debug:
+        case DEBUG:
             dbg_lvl_str = "DEBUG";
             stream = stdout;
             break;
-        case info:
+        case INFO:
             dbg_lvl_str = "INFO";
             stream = stdout;
             break;
-        case warning:
+        case WARNING:
             dbg_lvl_str = "WARNING";
             stream = stdout;
             break;
-        case error:
+        case ERROR:
             dbg_lvl_str = "ERROR";
             stream = stderr;
             break;
-        case fatal:
+        case FATAL:
             dbg_lvl_str = "FATAL";
             stream = stderr;
             break;
--- src/debug.h
+++ src/debug.h
@@ -13,21 +13,21 @@
 #include "cli.h"
 
 typedef enum {
-    debug,
-    info,
-    warning,
-    error,
-    fatal
+    DEBUG,
+    INFO,
+    WARNING,
+    ERROR,
+    FATAL
 } debug_level_t;
 
 void _log_str(debug_level_t dbg_lvl, const char* msg, ...);
 
 void debug_init(const config_t* conf);
 
-#define LOG_DEBUG(msg, ...)     _log_str(debug, msg, ##__VA_ARGS__)
-#define LOG_INFO(msg, ...)      _log_str(info, msg, ##__VA_ARGS__)
-#define LOG_WARNING(msg, ...)   _log_str(warning, msg, ##__VA_ARGS__)
-#define LOG_ERROR(msg, ...)     _log_str(error, msg, ##__VA_ARGS__)
-#define LOG_FATAL(msg, ...)     _log_str(fatal, msg, ##__VA_ARGS__)
+#define LOG_DEBUG(msg, ...)     _log_str(DEBUG, msg, ##__VA_ARGS__)
+#define LOG_INFO(msg, ...)      _log_str(INFO, msg, ##__VA_ARGS__)
+#define LOG_WARNING(msg, ...)   _log_str(WARNING, msg, ##__VA_ARGS__)
+#define LOG_ERROR(msg, ...)     _log_str(ERROR, msg, ##__VA_ARGS__)
+#define LOG_FATAL(msg, ...)     _log_str(FATAL, msg, ##__VA_ARGS__)
 
 #endif /* _CH8_SRC_DEBUG_H_ */
