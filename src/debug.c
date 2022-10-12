c7de97a610f7fa5cd330c6c1a7e9674e6d487339
Moritz Reinhard Hüllmann <mhuellmann@googlemail.com>
2022-10-12 18:59:15 +0200

    feat: add LOG_{DEBUG, INFO, WARNING, ERROR, FATAL} for logging events

--- src/debug.c
+++ src/debug.c
@@ -1,1 +1,56 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include <time.h>
+
 #include "debug.h"
+
+static debug_level_t min_dbg_lvl = fatal;
+
+void debug_init(const config_t* conf) {
+    min_dbg_lvl = conf->debug;
+
+    // cap debug level, we want to see fatal errors at all times
+    if (min_dbg_lvl > fatal)
+        min_dbg_lvl = fatal;
+
+    LOG_INFO("Log level initialized to %d.", min_dbg_lvl);
+}
+
+__attribute__((format(printf, 2, 3))) void _log_str(debug_level_t dbg_lvl, const char *msg, ...) {
+    if (dbg_lvl < min_dbg_lvl) return;
+
+    va_list args;
+    time_t rawtime;
+    struct tm * timeinfo;
+
+    va_start(args, msg);
+    time(&rawtime);
+    timeinfo = localtime(&rawtime);
+    char* time_str = asctime(timeinfo);
+    time_str[24] = '\0'; // terminate str early to eliminate newline (i hate this)
+
+    const char* dbg_lvl_str = "unknown";
+    switch (dbg_lvl) {
+        case debug:
+            dbg_lvl_str = "DEBUG";
+            break;
+        case info:
+            dbg_lvl_str = "INFO";
+            break;
+        case warning:
+            dbg_lvl_str = "WARNING";
+            break;
+        case error:
+            dbg_lvl_str = "ERROR";
+            break;
+        case fatal:
+            dbg_lvl_str = "FATAL";
+            break;
+        default:
+            break;
+    }
+    printf("%10s – %s – ", dbg_lvl_str, time_str);
+    vprintf(msg, args);
+    printf("\n");
+
+}
--- src/debug.h
+++ src/debug.h
@@ -10,6 +10,24 @@
 #ifndef _CH8_SRC_DEBUG_H_
 #define _CH8_SRC_DEBUG_H_
 
+#include "cli.h"
 
+typedef enum {
+    debug,
+    info,
+    warning,
+    error,
+    fatal
+} debug_level_t;
+
+void _log_str(debug_level_t dbg_lvl, const char* msg, ...);
+
+void debug_init(const config_t* conf);
+
+#define LOG_DEBUG(msg, ...)     _log_str(debug, msg, ##__VA_ARGS__)
+#define LOG_INFO(msg, ...)      _log_str(info, msg, ##__VA_ARGS__)
+#define LOG_WARNING(msg, ...)   _log_str(warning, msg, ##__VA_ARGS__)
+#define LOG_ERROR(msg, ...)     _log_str(error, msg, ##__VA_ARGS__)
+#define LOG_FATAL(msg, ...)     _log_str(fatal, msg, ##__VA_ARGS__)
 
 #endif /* _CH8_SRC_DEBUG_H_ */
