90042e064e5bd6defd38ceb5a4548eb59b6e5efc
Moritz Reinhard Hüllmann <mhuellmann@googlemail.com>
2022-10-13 14:29:52 +0200

    feat: make frequency an optional argument

--- src/cli.c
+++ src/cli.c
@@ -10,7 +10,28 @@
 
 #define MAX_HZ (1000000L)
 
-static const char* usage_str = "cretro [-d <0..5>] <HERTZ> <ROM>";
+static const char* usage_str = "cretro [-d <0..5>] [-f <FREQUENCY_HZ>] <ROM>";
+
+static void _handle_arg_frequency(config_t* conf, long frequency) {
+    // calculate delay from hertz input
+    LOG_DEBUG("Frequency [Hz] input specified: %ld.", frequency);
+
+    if (frequency > MAX_HZ) {
+        LOG_ERROR("Provided frequency %ld is higher than allowed frequency %ld. Used Fallback: %ld", frequency, MAX_HZ, MAX_HZ);
+        frequency = MAX_HZ;
+    }
+    else if (frequency <= 0) {
+        LOG_ERROR("Provided frequency %ld is lower or equal to 0. Used Fallback: %ld", frequency, MAX_HZ);
+        frequency = MAX_HZ;
+    }
+
+    long delay = MAX_HZ / frequency;
+    if (delay > INT_MAX)
+        LOG_FATAL("Delay is out of bounds: %d > %d", delay, INT_MAX);
+
+    conf->us_delay = (int) (delay);
+    LOG_INFO("Delay set to %d us.", conf->us_delay);
+}
 
 __attribute__((__const__)) config_t cli_config_default(void) {
     config_t conf = {
@@ -25,17 +46,20 @@ __attribute__((__const__)) config_t cli_config_default(void) {
 int cli_config_handle(config_t* const conf, int argc, char **argv) {
     int c;
 
-    if (argc < 3) {
+    if (argc < 2) {
         fprintf(stderr, "%s\n", usage_str);
         return EXIT_FAILURE;
     }
 
     // parse all options first
-    while ((c = getopt(argc, argv, "d:")) != -1) {
+    while ((c = getopt(argc, argv, "d:f:")) != -1) {
         switch (c) {
             case 'd':
                 conf->debug = (int) strtol(optarg, NULL, 10);
                 log_init(conf);
+                break;
+            case 'f':
+                _handle_arg_frequency(conf, strtol(optarg, NULL, 10));
                 break;
             default:
                 fprintf(stderr, "%s\n", usage_str);
@@ -46,32 +70,12 @@ int cli_config_handle(config_t* const conf, int argc, char **argv) {
     // parse the remaining options: <HERTZ> <ROM>
 
     // calculate delay from hertz input
-    long hertz = strtol(argv[optind++], NULL, 10);
-    LOG_DEBUG("Frequency [Hz] input specified: %ld.", hertz);
-
-    if (hertz > MAX_HZ) {
-        LOG_FATAL("Provided frequency %ld is higher than allowed frequency %ld", hertz, MAX_HZ);
-        return EXIT_FAILURE;
-    }
-    else if (hertz <= 0) {
-        LOG_FATAL("Provided frequency %ld is lower or equal to 0", hertz);
-        return EXIT_FAILURE;
-    }
 
     // delay
     if (optind >= argc) {
         LOG_FATAL("No frequency [Hz] has been specified!");
         return EXIT_FAILURE;
     }
-
-    long delay = MAX_HZ / hertz;
-    if (delay > INT_MAX) {
-        LOG_FATAL("Delay is out of bounds: %d > %d", delay, INT_MAX);
-        return EXIT_FAILURE;
-    }
-
-    conf->us_delay = (int) (delay);
-    LOG_INFO("Delay set to %d us.", conf->us_delay);
 
     // path to rom
     if (optind >= argc) {
